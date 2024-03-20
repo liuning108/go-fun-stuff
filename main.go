d855d5dd43773f1f88be66e6706dfaf9c3547558
Liu ning <122273014@qq.com>
2024-03-20 14:21:14 +0800

    goroutine Server

--- main.go
+++ main.go
@@ -5,31 +5,63 @@ import (
 	"time"
 )
 
-func main() {
-
-	msgch := make(chan string, 10)
-	msgch <- "A"
-	msgch <- "B"
-	msgch <- "C"
-
-	close(msgch)
-
-	// for {
-	// 	msg, ok := <-msgch
-	// 	if !ok {
-	// 		break
-	// 	}
-	// 	fmt.Println("msg", msg)
-	// }
-
-	for msg := range msgch {
-		fmt.Println(msg)
-	}
-	fmt.Println("Done")
-
+type Server struct {
+	quitch chan struct{} // 0 bytes
+	msgch  chan string
 }
 
-func fetchRes(n int) string {
-	time.Sleep(time.Second * 2)
-	return fmt.Sprintf("res %d", n)
+func NewServer() *Server {
+	return &Server{
+		quitch: make(chan struct{}),
+		msgch:  make(chan string),
+	}
+}
+
+func (s *Server) start() {
+	fmt.Printf("Server started\n")
+	s.loop()
+}
+
+func (s *Server) loop() {
+mainLoop:
+	for {
+		select {
+		case <-s.quitch:
+			break mainLoop
+		case msg := <-s.msgch:
+			s.handleMessage(msg)
+		default:
+		}
+	}
+	fmt.Println("server is shutting down gracefully ")
+}
+
+func (s *Server) handleMessage(msg string) {
+	fmt.Printf("Message received: %s\n", msg)
+}
+
+func (s *Server) SendMessage(msg string) {
+	s.msgch <- msg
+}
+
+func (s *Server) quit() {
+	fmt.Println("quit command ")
+	s.quitch <- struct{}{}
+}
+
+func main() {
+
+	server := NewServer()
+
+	go func() {
+
+		fmt.Println("Sleep()")
+		time.Sleep(5 * time.Second)
+		server.quit()
+
+	}()
+
+	server.loop()
+	fmt.Println("Programer  Orver")
+
 }
